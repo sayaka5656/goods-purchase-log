7c15334bbd1cdd4a26a705b5ec499bb859455f82
sayaka5656 <sayaka.happy.mail.8.6.7@docomo.ne.jp>
2026-08-30 23:48:10 +0900

    Publish goods purchase log web app

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('merch-log-v1').then(c => c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));

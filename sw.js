39171481ae05a021f31c55199459c3d9b568aee0
sayaka5656 <sayaka.happy.mail.8.6.7@docomo.ne.jp>
2026-09-03 02:33:44 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName='merch-log-v2';
+const cacheName='merch-log-v3';
 self.addEventListener('install', e => e.waitUntil(caches.open(cacheName).then(c => c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key=>key!==cacheName).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
